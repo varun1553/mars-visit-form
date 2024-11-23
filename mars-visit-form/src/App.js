9217d62af6a004a1144deacdbb15e900de7c5d1a
Varun MMahankali <varun.mahankali39@gmail.com>
2024-11-23 02:54:05 -0600

    installed the build folder for deploy

--- mars-visit-form/src/App.js
+++ mars-visit-form/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import MarsVisitForm from './components/MarsVisitForm'; // Import the MarsVisitForm component
-import './App.css'; // Optional: For custom styling if you want to include global styles
+import MarsVisitForm from './components/MarsVisitForm'; 
+import './App.css'; 
 
 
 const App = () => {
   return (
     <div className="App" style={{ backgroundColor: "#f9f9f9",height:"800px" }}>
-      <MarsVisitForm /> {/* Render the MarsVisitForm component */}
+      <MarsVisitForm /> 
     </div>
   );
 };
